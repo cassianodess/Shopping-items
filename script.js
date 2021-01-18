dbbf294edf052f695b0718ce4924dc6897e6a62b
Cassiano de Souza <cassianodess@hotmail.com>
2021-01-18 16:23:15 -0300

    adding files

---
+++ script.js
@@ -0,0 +1,22 @@
+var input = window.document.getElementById("txt")
+
+input.focus()
+
+var btn = window.document.getElementById("btn")
+
+var ul = window.document.querySelector("ul")
+
+
+btn.addEventListener("click", function () {
+
+    if (input.value.length > 0) {
+        input.focus()
+        var li = window.document.createElement("li")
+        li.appendChild(document.createTextNode(input.value))
+        ul.appendChild(li)
+
+        input.value = ""
+    } else {
+        alert("Please, add an item")
+    }
+})
